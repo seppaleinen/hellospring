dbf323d91afadc47ce433dfdd07eb98ec8dddc41
David Eriksson <david.h.eriksson@ica.se>
2018-07-26 15:20:11 +0200

    Init gatling configs

---
+++ hellogatling/README.md
@@ -0,0 +1,16 @@
+# Gatling
+
+This is for testing out gatling.
+
+Gatling is a tool for setting up performance tests.
+
+```bash
+# To start spring-boot and run gatling against it
+gradle gatlingRun
+
+# To run regular tests
+gradle clean build
+```
+
+### TODO
+* Maven setup
---
+++ hellogatling/src/test/java/se/david/labs/IntegrationTest.java
@@ -0,0 +1,40 @@
+package se.david.labs;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootContextLoader;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(loader = SpringBootContextLoader.class)
+public class IntegrationTest {
+    @LocalServerPort
+    private int port;
+
+    @Before
+    public void setup() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    public void testEndpoint() {
+        final String expectedJsonResponse = "{\"data\":\"Hello\"}";
+
+        given().get("/endpoint")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("", equalTo(new JsonPath(expectedJsonResponse).getMap("")));
+    }
+
+}
