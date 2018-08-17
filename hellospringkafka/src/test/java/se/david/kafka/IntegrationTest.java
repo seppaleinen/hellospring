658b596a50747929f086f5f45a684927dd0d6f04
David Eriksson <davideriksson@swedenmail.com>
2018-08-17 19:59:53 +0200

    Changed small stuff

--- hellospringkafka/src/test/java/se/david/kafka/IntegrationTest.java
+++ hellospringkafka/src/test/java/se/david/kafka/IntegrationTest.java
@@ -1,10 +1,11 @@
 package se.david.kafka;
 
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -13,7 +14,9 @@ import org.springframework.http.MediaType;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import se.david.kafka.consumer.Receiver;
 import se.david.kafka.producer.Sender;
 
@@ -24,11 +27,9 @@ import static org.awaitility.Awaitility.await;
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.Mockito.verify;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(
-        classes = Application.class,
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
-)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(loader = SpringBootContextLoader.class)
 @ActiveProfiles("test")
 @EmbeddedKafka(topics = "receiver.t", controlledShutdown = true, count = 1)
 @DirtiesContext
@@ -42,13 +43,13 @@ public class IntegrationTest {
     @Value("${topic.receiver}")
     private String topic;
 
-    @BeforeEach
-    void setup() throws Exception {
+    @Before
+    public void setup() {
         RestAssured.port = port;
     }
 
     @Test
-    void test() {
+    public void test() {
         given().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE).
                 when().get("/kafka/message")
                 .then()
@@ -61,7 +62,7 @@ public class IntegrationTest {
     }
 
     @Test
-    void testReceive() {
+    public void testReceive() {
         String message = "message";
 
         sender.send(topic, message);
