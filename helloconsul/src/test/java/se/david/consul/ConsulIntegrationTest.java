662deb2a980397f04780bfc87ec13b14b6dfe7e1
David Eriksson <davideriksson@swedenmail.com>
2018-08-20 21:40:05 +0200

    Fixed consul registration

--- helloconsul/src/test/java/se/david/consul/ConsulIntegrationTest.java
+++ helloconsul/src/test/java/se/david/consul/ConsulIntegrationTest.java
@@ -1,17 +1,21 @@
 package se.david.consul;
 
-import com.pszymczyk.consul.ConsulStarter;
+import com.pszymczyk.consul.ConsulProcess;
+import com.pszymczyk.consul.ConsulStarterBuilder;
 import com.pszymczyk.consul.junit.ConsulResource;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.hamcrest.CoreMatchers;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.given;
@@ -21,17 +25,18 @@ import static io.restassured.RestAssured.given;
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         classes = Application.class
 )
+@ActiveProfiles("test")
 public class ConsulIntegrationTest {
     @LocalServerPort
     private int port;
 
-//    @ClassRule
-//    public static final ConsulResource consul = new ConsulResource();
+    @ClassRule
+    public static final ConsulResource consul = new ConsulResource(8502);
 
     @Before
     public void before() {
         RestAssured.port = port;
-//        consul.reset();
+        consul.reset();
     }
 
     @Test
