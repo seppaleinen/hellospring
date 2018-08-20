33094604fe64d6ea2628990cd525b092bf93d2f3
David Eriksson <davideriksson@swedenmail.com>
2018-08-20 22:20:16 +0200

    Kind of registering service in discovery

--- helloconsul/discovery/src/test/java/se/david/consul/ConsulIntegrationTest.java
+++ helloconsul/discovery/src/test/java/se/david/consul/ConsulIntegrationTest.java
@@ -1,5 +1,7 @@
 package se.david.consul;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pszymczyk.consul.ConsulProcess;
 import com.pszymczyk.consul.ConsulStarterBuilder;
 import com.pszymczyk.consul.junit.ConsulResource;
@@ -17,6 +19,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 import static io.restassured.RestAssured.given;
 
@@ -37,6 +40,22 @@ public class ConsulIntegrationTest {
     public void before() {
         RestAssured.port = port;
         consul.reset();
+        registerService();
+    }
+
+    private void registerService() {
+        ConsulData data = new ConsulData("dc1",
+                "google",
+                "http://localhost:8089",
+                new ConsulData.ConsulService("register", 8089));
+
+        String url = String.format("http://localhost:%s/v1/catalog/register", consul.getHttpPort());
+
+        given().contentType(ContentType.JSON)
+                .body(data)
+                .put(url)
+                .then()
+                .statusCode(HttpStatus.OK.value());
     }
 
     @Test
@@ -58,5 +77,53 @@ public class ConsulIntegrationTest {
                 .statusCode(HttpStatus.OK.value())
                 .contentType(ContentType.JSON)
                 .body("message", CoreMatchers.equalTo("hello"));
+    }
+
+    public static class ConsulData {
+        private String Datacenter;
+        private String Node;
+        private String Address;
+        private ConsulService Service;
+
+        ConsulData(String DataCenter, String Node, String Address, ConsulService Service) {
+            this.Datacenter = DataCenter;
+            this.Node = Node;
+            this.Address = Address;
+            this.Service = Service;
+        }
+
+        public String getDatacenter() {
+            return Datacenter;
+        }
+
+        public String getNode() {
+            return Node;
+        }
+
+        public String getAddress() {
+            return Address;
+        }
+
+        public ConsulService getService() {
+            return Service;
+        }
+
+        public static class ConsulService {
+            private String Service;
+            private int Port;
+
+            public ConsulService(String Service, int Port) {
+                this.Service = Service;
+                this.Port = Port;
+            }
+
+            public String getService() {
+                return Service;
+            }
+
+            public int getPort() {
+                return Port;
+            }
+        }
     }
 }
