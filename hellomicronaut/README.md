11f3e807abf63851b36482101996f08304942b20
David Eriksson <davideriksson@swedenmail.com>
2018-08-26 12:51:18 +0200

    Added health endpoint

--- hellomicronaut/README.md
+++ hellomicronaut/README.md
@@ -15,4 +15,16 @@ mvn clean install
 * Fix maven setup
 * DB integration
 * @EventListener
-* "Deprecated Gradle features were used in this build, making it incompatible with Gradle 5.0"
+* "The following annotation processors were detected on the compile classpath:  
+    'io.micronaut.annotation.processing.TypeElementVisitorProcessor' and   
+    'io.micronaut.annotation.processing.PackageConfigurationInjectProcessor' and  
+    'io.micronaut.annotation.processing.BeanDefinitionInjectProcessor'.   
+ Detecting annotation processors on the compile classpath is deprecated and   
+ Gradle 5.0 will ignore them. Please add them to the annotation processor path instead.  
+  If you did not intend to use annotation processors,   
+  you can use the '-proc:none' compiler argument to ignore them."
+
+
+### Docs
+
+* https://docs.micronaut.io/latest/guide/index.html
--- hellomicronaut/src/test/java/se/david/labs/HelloControllerTest.java
+++ hellomicronaut/src/test/java/se/david/labs/HelloControllerTest.java
@@ -2,6 +2,8 @@ package se.david.labs;
 
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.runtime.server.EmbeddedServer;
 import org.junit.AfterClass;
@@ -10,6 +12,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class HelloControllerTest {
 
@@ -38,6 +41,13 @@ public class HelloControllerTest {
         HttpRequest<Object> request = HttpRequest.GET("/hello");
         String body = client.toBlocking().retrieve(request);
         assertNotNull(body);
-        assertEquals(body, "Hello World");
+        assertEquals("Hello World", body);
+    }
+
+    @Test
+    public void healthEndpoint() {
+        HttpRequest<Object> request = HttpRequest.GET("/health");
+        String response = client.toBlocking().retrieve(request);
+        assertEquals("{\"status\":\"UP\"}", response);
     }
 }
