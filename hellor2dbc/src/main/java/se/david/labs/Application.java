13474191623c85631ae64bfbe97ae2483c8eeec0
daveri <david.eriksson@avanza.se>
2019-07-05 13:36:33 +0200

    Start of something atleast

--- hellor2dbc/src/main/java/se/david/labs/Application.java
+++ hellor2dbc/src/main/java/se/david/labs/Application.java
@@ -1,21 +1,41 @@
 package se.david.labs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import se.david.labs.repository.NewEntityRepository;
+import se.david.labs.repository.entity.NewEntity;
 
 @SpringBootApplication
 @RestController
 public class Application {
+	@Autowired
+	private NewEntityRepository newEntityRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping(value = "/endpoint", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	ResponseEntity<SuperDto> call() {
-		return ResponseEntity.ok(new SuperDto("Hello"));
+	@PostMapping(path = "/reactive", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	Mono<NewEntity> createOne(NewEntity newEntity) {
+		return newEntityRepository.save(newEntity);
+	}
+
+	@GetMapping(path = "/reactive/{id}")
+	Mono<NewEntity> mono(@PathVariable("id") Long id){
+		return newEntityRepository.findById(id);
+	}
+
+	@GetMapping(path = "/reactive")
+	Flux<NewEntity> getAll(){
+		return newEntityRepository.findAll();
 	}
 }
---
+++ hellor2dbc/src/main/java/se/david/labs/repository/entity/NewEntity.java
@@ -0,0 +1,42 @@
+package se.david.labs.repository.entity;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class NewEntity {
+    @Id
+    private Long id;
+
+    public NewEntity() {}
+
+    public NewEntity(Long id) {
+            this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NewEntity newEntity = (NewEntity) o;
+        return Objects.equals(id, newEntity.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "NewEntity{" +
+                "id=" + id +
+                '}';
+    }
+}
